39e9e345cf8a422e9f318ed2b392d81d73d7589c
mercadojonathan <mercadojonathan1@yahoo.com>
2016-12-08 19:26:08 -0600

    Update Collect ScreenNames.py

--- Collect ScreenNames.py
+++ Collect ScreenNames.py
@@ -1,43 +1,25 @@
-#|---------------------------------------------------------|
-#| IMPORT                                                                      |
-#|---------------------------------------------------------|
+# Collect tweets from users tweeting about stock in Stock List
+# Save Twitter handles to S
+
 from twython import Twython, TwythonError, TwythonStreamer
-import sys
-import time
-import datetime as dt
-from textblob import TextBlob
-from yahoo_finance import Share
-#|--------------------------------------------------------|
-#| OATH KEYS                                                               |
-#|--------------------------------------------------------|
+
 APP_KEY= '********'
 APP_SECRET= '********'
 OAUTH_TOKEN= '********'
 OAUTH_TOKEN_SECRET= '********'
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-#|-------------------------------------------------------|
-#| STOCK LISTS                                                           |
-#|-------------------------------------------------------|
-stock_list = '$FB,$BAC,$TWTR,$X,$MU,$SBUX,$WLL,$AMD,$TWLO,$HPQ,$OAS,$ECA,$TCK,$WFM,$P,$JCP,$FIT,$JBLU,$CNX,$GPRO,$SCTY,$SQ,$SHOP,$ETSY,$MPEL,$REN'
-#|--------------------------------------------------------|
-#| STRING VARIABLES                                                 |
-#|--------------------------------------------------------|
+
+stock_list = '$FB,$BAC,$TWTR,$X,$MU,$SBUX,$WLL,$AMD,$TWLO,$HPQ,$OAS,$ECA,$TCK,$P,$JCP,$FIT,$JBLU,$CNX,$GPRO,$SCTY,$SQ,$SHOP,$ETSY,$MPEL'
 keyword = "What would you like to Stream?\n"
-#|-------------------------------------------------------|
-#| STREAM & SAVE FUNCTION                                |
-#|-------------------------------------------------------|
-handles = []
+
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
             try:
-                #print(data['text'])     #.encode('utf-8'))
                 global name
                 name = data['user']['screen_name']
-                print(name)
-                #date = str(dt.datetime.now())
-                save_data = name#date + '\n' + data['text'] + '\n' + name #.encode('utf-8')
-                saveFile = open('STREAM.json', 'a') #a = apphend the data
+                save_data = name
+                saveFile = open('STREAM.json', 'a')
                 saveFile.write(save_data)
                 saveFile.write('\n')
                 saveFile.close()
@@ -45,34 +27,30 @@ class MyStreamer(TwythonStreamer):
                 return True
             
             except BaseException:
-                print('Failed on Data\n')
-                time.sleep(2)
+                print('Write Data Failed\n')
             
     def on_error(self, status_code, data):
         print(status_code, data)
 
 stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-def stream_filter(): # define streaming function
+def stream_filter():
     key_word= input(keyword)
-    
     if(key_word == 'stock list'):
         status = stream.statuses.filter(track=stock_list)
     else:
         status = stream.statuses.filter(track=key_word)
 
 # Create Function that opens handles list and follows each target
-def follow_targ():
+def follow_from_file():
+    global read_file
     read_file = open('STREAM.json', 'r')
-    read_file.read()
-    follow_target = input(request_target)
-    twitter.create_friendship(screen_name=follow_target, follow="true")
-    print('\nNow Following... ' + follow_target +'!\n')
+    handle_list = read_file.readlines()
+    for each_handle in handle_list:
+        twitter.create_friendship(screen_name=each_handle, follow="true")
+        print('\nNow Following... ' + each_handle +'!\n')
     
 
 stream_filter()
-#print(handles)
-# stream.user()
-# Read the authenticated users home timeline
-# (what they see on Twitter) in real-time
-# stream.site(follow='twitter')
+stream.user()
+follow_from_file()
+
